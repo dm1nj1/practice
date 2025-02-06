4a7cd9614722ad6af4e217de882ddc9759c94de4
nua1030 <gumdong1030@gmail.com>
2025-02-06 14:05:17 +0900

    ZERO

--- jena/3.Reviews/Capp.js
+++ jena/3.Reviews/Capp.js
@@ -30,7 +30,7 @@ const reviews = [
 ];
 // select items
 const img = document.getElementById("person-img");
-const author = document.getElementById("auther");
+const author = document.getElementById("author");
 const job = document.getElementById("job");
 const info = document.getElementById("info");
 
